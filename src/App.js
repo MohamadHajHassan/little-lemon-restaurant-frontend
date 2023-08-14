e4d4dbf5e1a390ac35030890584e85c603b37c0c
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-08-14 17:10:46 +0300

    initialize the header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
     return (
         <>
-            <header>Header</header>
+            <Header />
             <nav>Nav</nav>
             <main>Main</main>
             <footer>Footer</footer>
