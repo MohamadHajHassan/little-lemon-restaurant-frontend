d86fed6313b1a3926ebcbcaf371ac5436e084a2f
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-08-20 22:03:57 +0300

    add navigation elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "assets/logo.png";
 import "components/styles/header.css";
+import Nav from "./Nav";
 
 const Header = () => {
     return (
         <header>
             <img src={logo} alt="Little Lemon Logo" />
+            <Nav />
         </header>
     );
 };
