a87bad22df6bc7618206ff05325cdbea5dc87328
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-10-29 13:15:04 +0200

    add hero elements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import restaurantFoodImage from "../assets/restaurant-food.jpg";
 
 const Hero = () => {
-    return <div>Hero</div>;
+    return (
+        <section>
+            <div>
+                <h1>Little Lemon</h1>
+                <h2>Chicago</h2>
+                <p>
+                    We are a family owned Mediterranean restaurant, focused on
+                    traditional recipes served with a modern twist.
+                </p>
+            </div>
+            <img src={restaurantFoodImage} alt="Restaurant food" />
+        </section>
+    );
 };
 
 export default Hero;
