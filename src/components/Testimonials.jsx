34b64621d2d467c1165f41fe3f0075e6d0ae2fea
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-11-28 23:14:19 +0200

    add and style testimonials

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import customer1Image from "assets/customer1.jpg";
+import customer2Image from "assets/customer2.jpg";
+import customer3Image from "assets/customer3.jpg";
+import customer4Image from "assets/customer4.jpg";
+import TestimonialCard from "./TestimonialCard";
+import "./styles/testimonials.css";
+
+const customers = [
+    {
+        name: "Jane Doe",
+        image: customer1Image,
+        says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    },
+    {
+        name: "John Doe",
+        image: customer2Image,
+        says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    },
+    {
+        name: "Jane Doe",
+        image: customer3Image,
+        says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    },
+    {
+        name: "John Doe",
+        image: customer4Image,
+        says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    },
+];
+
+const Testimonials = () => {
+    return (
+        <section className="testimonials">
+            <div className="container grid">
+                <h2>What people say about us!</h2>
+                {customers.map((customer, index) => (
+                    <TestimonialCard key={index} customer={customer} />
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default Testimonials;
