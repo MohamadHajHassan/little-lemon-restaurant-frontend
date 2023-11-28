e3d762405075714e6670a0972fd3d2ad6a85acc4
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-11-28 20:25:19 +0200

    add specials elements

--- src/components/Specials.jsx
+++ src/components/Specials.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import greekSaladImage from "../assets/greek-salad.jpg";
+import bruschettaImage from "../assets/bruschetta.jpg";
+import lemonDessertImage from "../assets/lemon-dessert.jpg";
+import DishCard from "./DishCard";
+
+const dishes = [
+    {
+        name: "Greek Salad",
+        image: greekSaladImage,
+        price: "$12.99",
+        description: `The famous greek salad of crispy lettuce, peppers, olives and 
+        our Chicago style feta cheese, garnished with crunchy garlic and rosemary 
+        croutons.`,
+    },
+    {
+        name: "Bruschetta",
+        image: bruschettaImage,
+        price: "$5.99",
+        description: `Our Bruschetta is made from grilled bread that has been 
+        smeared with garlic and seasoned with salt and olive oil.`,
+    },
+    {
+        name: "Lemon Dessert",
+        image: lemonDessertImage,
+        price: "$5.00",
+        description: `This comes straight from grandma's recipe book, every last 
+        ingredient has been sourced and is as authentic as can be imagined.`,
+    },
+];
 
 const Specials = () => {
-    return <div>Specials</div>;
+    return (
+        <section>
+            <div>
+                <h2>This week specials!</h2>
+                <p>Online Menu</p>
+            </div>
+            {dishes.map((dish, index) => (
+                <DishCard key={index} dish={dish} />
+            ))}
+        </section>
+    );
 };
 
 export default Specials;
