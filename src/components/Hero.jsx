e83391993f4a63b8c3967853c6cbd9842b60ad49
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-11-28 17:35:25 +0200

    style hero elements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import restaurantFoodImage from "../assets/restaurant-food.jpg";
+import "components/styles/hero.css";
 
 const Hero = () => {
     return (
-        <section>
-            <div>
-                <h1>Little Lemon</h1>
-                <h2>Chicago</h2>
-                <p>
-                    We are a family owned Mediterranean restaurant, focused on
-                    traditional recipes served with a modern twist.
-                </p>
+        <section className="hero">
+            <div className="container grid">
+                <div className="hero-info">
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p>
+                        We are a family owned Mediterranean restaurant, focused
+                        on traditional recipes served with a modern twist.
+                    </p>
+                </div>
+                <img
+                    className="hero-image"
+                    src={restaurantFoodImage}
+                    alt="Restaurant food"
+                />
             </div>
-            <img src={restaurantFoodImage} alt="Restaurant food" />
         </section>
     );
 };
