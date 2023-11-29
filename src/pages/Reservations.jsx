7f4a3ca3cda9d5c42e821e33cd2976c4199fc2d0
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-11-29 21:09:21 +0200

    initialize BookingForm component

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import BookingForm from "components/BookingForm";
 
 const Reservations = () => {
-    return <div>Reservations</div>;
+    return <BookingForm />;
 };
 
 export default Reservations;
