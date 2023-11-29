d8b8030a3c799812089e56f93b60692c49287a87
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-11-29 13:08:39 +0200

    refactor routing and add new pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import "./App.css";
+import Home from "pages/Home";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Main from "./components/Main";
+import { Routes, Route } from "react-router-dom";
+import UnderConstruction from "pages/UnderConstruction";
+import Reservations from "pages/Reservations";
 
 function App() {
     return (
         <>
             <Header />
-            <Main />
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<UnderConstruction />} />
+                    <Route path="/menu" element={<UnderConstruction />} />
+                    <Route path="/reservations" element={<Reservations />} />
+                    <Route
+                        path="/order-online"
+                        element={<UnderConstruction />}
+                    />
+                    <Route path="/login" element={<UnderConstruction />} />
+                </Routes>
+            </main>
             <Footer />
         </>
     );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Specials from "../components/Specials";
+import Testimonials from "../components/Testimonials";
+import AboutUs from "../components/AboutUs";
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <Specials />
+            <Testimonials />
+            <AboutUs />
+        </>
+    );
+};
+
+export default Home;
