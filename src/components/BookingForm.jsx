60ec671b8ff1d3b387b929fb4c8ccdb09fc8fa55
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-11-30 15:38:52 +0200

    add form functionality to BookingForm component

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,21 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BookingForm = () => {
+    const occasions = ["Birthday", "Anniversary"];
+    const [availableTimes, setAvailableTimes] = useState([
+        { label: "17:00", value: "17:00" },
+        { label: "18:00", value: "18:00" },
+        { label: "19:00", value: "19:00" },
+        { label: "20:00", value: "20:00" },
+        { label: "21:00", value: "21:00" },
+        { label: "22:00", value: "22:00" },
+    ]);
+
+    const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
+    const [time, setTime] = useState(availableTimes[0]);
+    const [guests, setGuests] = useState(1);
+    const [occasion, setOccasion] = useState(occasions[0]);
+
+    const handleDateChange = e => {
+        setDate(e.target.value);
+    };
+
+    const handleTimeChange = e => {
+        setTime(e.target.value);
+    };
+
+    const handleGuestsChange = e => {
+        setGuests(e.target.value);
+    };
+
+    const handleOccasionChange = e => {
+        setOccasion(e.target.value);
+    };
+
+    const handleFormSubmit = e => {
+        e.preventDefault();
+    };
+
     return (
-        <form>
+        <form onSubmit={handleFormSubmit}>
             <div>
                 <label htmlFor="res-date">Choose date</label>
-                <input type="date" id="res-date" />
+                <input
+                    type="date"
+                    id="res-date"
+                    value={date}
+                    onChange={handleDateChange}
+                />
             </div>
             <div>
                 <label htmlFor="res-time">Choose time</label>
-                <select id="res-time ">
-                    <option>17:00</option>
-                    <option>18:00</option>
-                    <option>19:00</option>
-                    <option>20:00</option>
-                    <option>21:00</option>
-                    <option>22:00</option>
+                <select id="res-time" value={time} onChange={handleTimeChange}>
+                    {availableTimes.map(time => (
+                        <option key={time.value} value={time.value}>
+                            {time.label}
+                        </option>
+                    ))}
                 </select>
             </div>
             <div>
@@ -26,13 +65,21 @@ const BookingForm = () => {
                     min="1"
                     max="10"
                     id="guests"
+                    value={guests}
+                    onChange={handleGuestsChange}
                 />
             </div>
             <div>
                 <label htmlFor="occasion">Occasion</label>
-                <select id="occasion">
-                    <option>Birthday</option>
-                    <option>Anniversary</option>
+                <select
+                    id="occasion"
+                    value={occasion}
+                    onChange={handleOccasionChange}>
+                    {occasions.map(occasion => (
+                        <option key={occasion} value={occasion}>
+                            {occasion}
+                        </option>
+                    ))}
                 </select>
             </div>
             <div>
