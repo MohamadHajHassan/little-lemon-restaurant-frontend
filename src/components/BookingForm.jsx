52dcae99ba01dcd6a6e1d33767718aba0740557a
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-11-30 14:49:10 +0200

    Add form structure to BookingForm component

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
 const BookingForm = () => {
-    return <div>BookingForm</div>;
+    return (
+        <form>
+            <div>
+                <label htmlFor="res-date">Choose date</label>
+                <input type="date" id="res-date" />
+            </div>
+            <div>
+                <label htmlFor="res-time">Choose time</label>
+                <select id="res-time ">
+                    <option>17:00</option>
+                    <option>18:00</option>
+                    <option>19:00</option>
+                    <option>20:00</option>
+                    <option>21:00</option>
+                    <option>22:00</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="guests">Number of guests</label>
+                <input
+                    type="number"
+                    placeholder="1"
+                    min="1"
+                    max="10"
+                    id="guests"
+                />
+            </div>
+            <div>
+                <label htmlFor="occasion">Occasion</label>
+                <select id="occasion">
+                    <option>Birthday</option>
+                    <option>Anniversary</option>
+                </select>
+            </div>
+            <div>
+                <input type="submit" value="Make Your reservation" />
+            </div>
+        </form>
+    );
 };
 
 export default BookingForm;
