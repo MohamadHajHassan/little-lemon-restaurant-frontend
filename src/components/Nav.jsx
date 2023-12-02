9174e33870b306237aea9e5fbfac174f8d2faf2e
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-12-02 15:44:26 +0200

    add hamburger menu and responsiveness to header

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,27 +1,55 @@
-import React from "react";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Nav = ({ navStyle }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleHamburgerMenu = () => {
+        setIsOpen(prevState => !prevState);
+    };
+
     return (
-        <nav>
-            <ul className={navStyle}>
+        <nav className={navStyle === "navbar" ? "nav" : ""}>
+            {navStyle === "navbar" && (
+                <div className="hamburger-menu" onClick={toggleHamburgerMenu}>
+                    <FontAwesomeIcon
+                        icon={isOpen ? faTimes : faBars}
+                        size="2x"
+                    />
+                </div>
+            )}
+            <ul className={`${navStyle} ${navStyle === "navbar"? isOpen ? "open" : "closed":'' }`}>
                 <li>
-                    <Link to="/">HOME</Link>
+                    <Link to="/" onClick={toggleHamburgerMenu}>
+                        HOME
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/about">ABOUT</Link>
+                    <Link to="/about" onClick={toggleHamburgerMenu}>
+                        ABOUT
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/menu">MENU</Link>
+                    <Link to="/menu" onClick={toggleHamburgerMenu}>
+                        MENU
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/reservations">RESERVATIONS</Link>
+                    <Link to="/reservations" onClick={toggleHamburgerMenu}>
+                        RESERVATIONS
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/order-online">ORDER ONLINE</Link>
+                    <Link to="/order-online" onClick={toggleHamburgerMenu}>
+                        ORDER ONLINE
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/login">LOGIN</Link>
+                    <Link to="/login" onClick={toggleHamburgerMenu}>
+                        LOGIN
+                    </Link>
                 </li>
             </ul>
         </nav>
